628be3e811063498a490e94e3c143144a1a4088a
Bonnie <bonnie.wong@and.digital>
2021-09-26 20:24:08 +0100

    working app, add and remove tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,14 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food shooping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-]
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map((task) => (
-                <p>{task.text}</p>
+                <Task
+                    key={task.id}
+                    task={task}
+                    onDelete={onDelete}
+                    onToggle={onToggle} />
             ))}
         </>
     )
